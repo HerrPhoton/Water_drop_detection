b8d665019b4a54d5689e2215974e866bcd43f5e0
Sanya <aleisle@mail.ru>
2023-06-23 01:22:01 +0700

    Добавил тесты

---
+++ tests/test_SaveBatch.py
@@ -0,0 +1,40 @@
+import sys
+
+from os.path import dirname, join, abspath
+sys.path.insert(0, abspath(join(dirname(__file__), '..')))
+
+import pytest
+from src.NN.SaveBatch import *
+
+##################################### Testing DropDataset #####################################
+def test_DropDataset():
+
+    dataset = DropDataset('dataset', '.jpg', '.npy')
+
+    assert len(dataset) == 4 and len(dataset.__getitem__(0)) == 2
+
+@pytest.mark.parametrize("dir, img_suf, mask_suf, error",
+                         [('error', '.jpg', '.npy', FileNotFoundError),
+                          ('folder_with_junk', '.jpg', '.npy', Exception),
+                          ('folder_with_junk', '.png', '.npy', FileNotFoundError),
+                          ('folder_with_junk', '.jpg', '.png', FileNotFoundError)])
+def test_error_DropDataset(dir, img_suf, mask_suf, error):
+
+    with pytest.raises(error):
+        DropDataset(dir, img_suf, mask_suf)
+
+##################################### Testing SaveToFile #####################################
+def test_SaveToFile():
+    dataset = DropDataset('dataset', '.jpg', '.npy')
+    SaveToFile(dataset, '.', 'dataset')
+
+    assert os.path.isfile('dataset.npz')
+
+    os.remove('dataset.npz')
+
+def test_error_SaveToFile():
+
+    dataset = 42
+
+    with pytest.raises(TypeError):
+        SaveToFile(dataset, '.', 'dataset')
