a6fca7506b5734f02b66213fe06325794a9c94e8
Sanya <aleisle@mail.ru>
2023-06-23 22:43:37 +0700

    Add realpath

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("requirements.txt", "r") as file:
 
 setuptools.setup(
     name = "water_drop_detection",
-    version = "0.7",
+    version = "0.8",
     author = "Aleksandr Leisle, Sofia Volodina, Alina Shitenko",
     author_email = "a.leisle@g.nsu.ru, s.volodina@g.nsu.ru, a.shitenko@g.nsu.ru",
     description = "A neural network that detects water drops on the surface from an image.",
@@ -19,7 +19,7 @@ setuptools.setup(
         'Bug Tracker': 'https://github.com/HerrPhoton/Water_drop_detection/issues',
         'Homepage': 'https://github.com/HerrPhoton/Water_drop_detection',
     },
-    packages = ['water_drop_detection', 'water_drop_detection/NN', 'water_drop_detection/ui'],
+    packages = ['water_drop_detection'],
     install_requires = requirements,
     classifiers = [
         "Programming Language :: Python :: 3.9",
--- water_drop_detection/main.py
+++ water_drop_detection/main.py
@@ -1,3 +1,8 @@
+import sys
+
+from os.path import dirname, realpath
+sys.path.insert(0, dirname(realpath(__file__)))
+
 from ui.GUI import main
 
 if __name__ == "__main__":
