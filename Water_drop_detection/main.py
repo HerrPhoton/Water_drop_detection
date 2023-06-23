cfc8f313dbc998406deaa70b3224d01f4d816526
Sanya <aleisle@mail.ru>
2023-06-23 21:20:13 +0700

    Исправляю баг

--- Water_drop_detection/main.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-from os.path import dirname, join, abspath
-sys.path.insert(0, abspath(join(dirname(__file__), '..')))
-
-from water_drop_detection.ui.GUI import main
-
-if __name__ == "__main__":
-    main() 
